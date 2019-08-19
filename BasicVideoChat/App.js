60907b7742b76b186987f2fa9a95c6fb2adf8dde
Ben Allen <me@benallen.info>
2019-08-19 13:52:00 +0100

    Crash code

--- BasicVideoChat/App.js
+++ BasicVideoChat/App.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import { OTSession, OTPublisher, OTSubscriber } from 'opentok-react-native';
+import React, { Component } from "react";
+import { View, Button } from "react-native";
+import { OTSession, OTPublisher, OTSubscriber } from "opentok-react-native";
 
 export default class App extends Component {
+  state = {
+    mounted: false,
+    clientMounted: false,
+    hostMounted: false
+  };
   constructor(props) {
     super(props);
-    this.apiKey = '';
-    this.sessionId = '';
-    this.token = '';
+    this.apiKey = "XXXX";
+    this.sessionId = "XXXX";
+    this.clientToken = "XXXX";
+    this.hostToken = "XXXX";
   }
   render() {
     return (
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-        <OTSession apiKey={this.apiKey} sessionId={this.sessionId} token={this.token}>
-          <OTPublisher style={{ width: 100, height: 100 }} />
-          <OTSubscriber style={{ width: 100, height: 100 }} />
-        </OTSession>
+      <View>
+        <Button
+          title="Client Mount"
+          onPress={() =>
+            this.setState(state => ({ clientMounted: !state.clientMounted }))
+          }
+        />
+        <Button
+          title="Host Mount"
+          onPress={() =>
+            this.setState(state => ({ hostMounted: !state.hostMounted }))
+          }
+        />
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          {this.state.clientMounted && (
+            <OTSession
+              apiKey={this.apiKey}
+              sessionId={this.sessionId}
+              token={this.clientToken}
+            >
+              <OTSubscriber style={{ width: 100, height: 100 }} />
+            </OTSession>
+          )}
+          {this.state.hostMounted && (
+            <OTSession
+              apiKey={this.apiKey}
+              sessionId={this.sessionId}
+              token={this.hostToken}
+            >
+              <OTPublisher style={{ width: 100, height: 100 }} />
+            </OTSession>
+          )}
+        </View>
       </View>
     );
   }
